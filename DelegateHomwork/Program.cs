b3d790dc9320aadd753febca415bb0dec61a014c
Tural <74408075+Tural072@users.noreply.github.com>
2021-05-16 15:26:17 +0400

    Add files via upload

---
+++ DelegateHomwork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input word : ");
+            string word = Console.ReadLine();
+            MyClass myClass = new MyClass();
+            Func funcDel = new Func(myClass.Reverse);
+            funcDel += myClass.Space;
+            Run run = new Run();
+            run.RunF(funcDel, word);
+        }
+    }
+}
---
+++ DelegateHomwork/Run.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHomework
+{
+    class Run
+    {
+        public void RunF(Func func,string word) {
+            func.Invoke(word);
+        }
+    }
+}
